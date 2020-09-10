663558cea00b50118901530e4926c8c87c5069d5
Rick Viscomi <rviscomi@gmail.com>
2020-09-10 13:49:13 -0400

    countDeclarations

--- js/08-sizing-model.js
+++ js/08-sizing-model.js
@@ -1,6 +1,5 @@
 // How many instances of box-sizing:border-box are there?
 
 export default function countBorderBoxDeclarations(ast) {
-  const declarations = countDeclarationsByProperty(ast.stylesheet.rules, {properties: /^(-(o|moz|webkit|ms)-)?box-sizing$/, values: 'border-box'});
-  return declarations['box-sizing'] || 0;
+  return countDeclarations(ast.stylesheet.rules, {properties: /^(-(o|moz|webkit|ms)-)?box-sizing$/, values: 'border-box'});
 }
