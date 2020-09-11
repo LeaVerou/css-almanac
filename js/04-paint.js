0618de40f3377fcce4523ff7ca4a9ff62f06f3fb
Lea Verou <lea@verou.me>
2020-09-12 02:27:25 +0300

    Create 04-paint.js
    Rel #4

---
+++ js/04-paint.js
@@ -0,0 +1,21 @@
+export default function compute() {
+// Returns object of paint worklet names.
+// Object.keys(ret).length > 0 can give us a plain boolean on whether the Paint API is used on the website.
+let ret = {};
+
+walkDeclarations(ast, ({property, value}) => {
+	for (let paint of extractFunctionCalls(value, {names: "paint"})) {
+		let name = paint.args.match(/^[-\w+]+/)[0];
+
+		if (name) {
+			ret[name] = (ret[name] || 0) + 1;
+		}
+	}
+}, {
+	properties: /^--|-image$|^background$|^content$/,
+	values: /\bpaint\(/
+});
+
+return sortObject(ret);
+
+}
