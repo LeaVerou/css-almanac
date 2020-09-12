3c7a6c99013e54f6fb4e46758fa4b785804cee6d
Lea Verou <lea@verou.me>
2020-09-12 03:42:16 +0300

    Create 09-prefixes.js
    Rel #9

---
+++ js/09-prefixes.js
@@ -0,0 +1,61 @@
+export default function compute() {
+	let ret = {
+		pseudoClasses: {},
+		pseudoEls: {},
+		properties: {},
+		functions: {},
+		keywords: {},
+		media: {}
+	};
+
+	walkRules(ast, rule => {
+		// Prefixed pseudos
+		if (rule.selectors) {
+			let pseudos = rule.selectors.flatMap(r => r.match(/::?-[a-z]+-[\w-]+/g) || []);
+
+			for (let pseudo of pseudos) {
+				let type = "pseudo" + (pseudo.indexOf("::") === 0? "Els" : "Classes");
+				incrementByKey(ret[type], pseudo);
+			}
+		}
+
+		if (rule.declarations) {
+			walkDeclarations(rule, ({property, value}) => {
+				// Prefixed properties
+				if (/^-[a-z]+-.+/.test(property)) {
+					incrementByKey(ret.properties, property);
+				}
+
+				// -prefix-function()
+				for (let call of extractFunctionCalls(value, {names: /^-[a-z]+-.+/})) {
+					incrementByKey(ret.functions, call.name);
+				}
+
+				// Prefixed keywords
+				value.match(/(?<![-a-z])-[a-z]+-[a-z-]+(?=;|\s|,|\/)/g)?.forEach(k => incrementByKey(ret.keywords, k));
+			});
+		}
+
+		// Prefixed media features
+		if (rule.media) {
+			let features = rule.media
+								.replace(/\s+/g, "")
+								.match(/\(-[a-z]+-[\w-]+(?=[:\)])/g)
+								?.map(s => s.slice(1));
+
+			if (features) {
+				for (let feature of features) {
+					incrementByKey(ret.media, feature);
+				}
+			}
+		}
+
+
+	});
+
+	for (let type in ret) {
+		ret[type] = sortObject(ret[type]);
+	}
+
+	return ret;
+}
