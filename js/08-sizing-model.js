cb5d628bc98e1c625465bc84bca765cda2a80bd4
Lea Verou <lea@verou.me>
2020-09-16 16:40:20 +0300

    Update 08-sizing-model.js
    Syntax consistency

--- js/08-sizing-model.js
+++ js/08-sizing-model.js
@@ -1,5 +1,8 @@
 // How many instances of box-sizing:border-box are there?
 
 export default function countBorderBoxDeclarations(ast) {
-  return countDeclarations(ast.stylesheet.rules, {properties: /^(-(o|moz|webkit|ms)-)?box-sizing$/, values: 'border-box'});
+	return countDeclarations(ast, {
+		properties: /^(-(o|moz|webkit|ms)-)?box-sizing$/,
+		values: "border-box"
+	});
 }
