40603c8bfa79dbf0532320bcca3bec658a22040d
Lea Verou <lea@verou.me>
2020-09-22 14:51:43 +0300

    Update 26-property-groups.js

--- js/26-property-groups.js
+++ js/26-property-groups.js
@@ -1,4 +1,5 @@
 export default function compute() {
+
 // let properties = [];
 //
 // walkRules(ast, rule => {
@@ -11,40 +12,57 @@ export default function compute() {
 // return result.map(a => { return {lhs: a.lhs, rhs: a.rhs}})
 
 let usedTogether = {};
+let rules = [];
 
 walkRules(ast, rule => {
-	let props = new Set();
+	let ruleProps = rule.declarations
+			// Ignore custom properties and prefixed ones. CP because they don't generalize
+			// and prefixed ones because they will trivially correlate with each other and the non-prefixed version
+			.filter(d => !d.property.startsWith("-"))
+			.map(d => d.property);
 
-	for (let d of rule.declarations) {
-		props.add(d.property)
-	}
-
-	for (let prop of props) {
-		usedTogether[prop] = usedTogether[prop] || {};
-
-		for (let prop2 of props) {
-			if (prop === prop2) {
-				continue;
-			}
-
-			incrementByKey(usedTogether[prop], prop2);
-		}
-	}
-
+	let props = new Set(ruleProps);
+	rules.push(props);
 }, {rules: r => r.declarations});
 
-// Now sort by usage count
-for (let prop in usedTogether) {
-	let obj = usedTogether[prop];
+for (let props of rules) {
+	for (let prop of props) {
+		usedTogether[prop] = usedTogether[prop] || {total: 0};
+		usedTogether[prop].total++;
 
-	// Remove properties that are only used together once
-	for (let p in obj) {
-		if (obj[p] === 1) {
-			delete obj[p];
+		for (let prop2 of props) {
+			if (prop !== prop2) {
+				usedTogether[prop][prop2] = usedTogether[prop][prop2] || 0;
+				usedTogether[prop][prop2]++;
+			}
+		}
+	}
+}
+
+
+for (let property in usedTogether) {
+	let obj = usedTogether[property];
+	let total = obj.total.length;
+
+	for (let prop in obj) {
+		if (prop === "total") {
+			continue;
+		}
+
+		let f = obj[prop];
+
+		if (f < Math.max(3, obj.total * .15)) {
+			// Prune those appearing together in less than 15% of rules or in fewer than 3 rules
+			delete obj[prop];
 		}
 	}
 
-	let sortedEntries = sortObject(obj);
+	if (Object.keys(obj).length <= 1) {
+		delete usedTogether[property];
+	}
+	else {
+		usedTogether[property] = sortObject(obj);
+	}
 }
 
 return usedTogether;
